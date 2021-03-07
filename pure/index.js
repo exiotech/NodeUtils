51b8f2b537eb596308a5cba9a835831c357ba3cb
AndoGhevian <lenyvonbeer@gmail.com>
2021-03-07 22:29:09 +0400

    allSettled changed, tested

--- pure/index.js
+++ pure/index.js
@@ -1,3 +1,5 @@
+// const allSettledShim = require('promise.allsettled')
+
 async function sleep(time = 1000) {
     return new Promise((res) => {
         setTimeout(res, time);
@@ -21,28 +23,31 @@ function isNonNegativeInt(val, {
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 async function allSettled(promises) {
-    if(!(promises instanceof Array)) {
+    if (!(promises instanceof Array)) {
         throw new Error(`Promises Must be array of promises`)
     }
 
     const settled = await Promise.allSettled(promises)
-    return settled.reduce((map, p) => {
-        switch (p.status) {
-            case FULFILLED:
-                map[FULFILLED].push(p.value)
-                break
-            case REJECTED:
-                map[REJECTED].push(p.reason)
-                break
-            default:
-        }
 
-        return map
-    }, {
+    const mapped = {
         [FULFILLED]: [],
         [REJECTED]: []
-    })
+    }
+    for (const p of settled) {
+        let value
+        if (p.status === FULFILLED) {
+            value = await p.value
+            mapped[FULFILLED].push(value)
+        } else if (p.status === REJECTED) {
+            value = await p.reason
+            mapped[REJECTED].push(value)
+        }
+    }
+
+    return mapped
 }
+
+allSettled([Promise.resolve({name: 'aaaa'}), Promise.reject(2)])
 
 module.exports = {
     sleep,
